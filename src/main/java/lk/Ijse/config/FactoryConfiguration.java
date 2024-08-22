25250c11844cf5ed769a9989334ab292e130ef42
Nethmi <kaveefernando2003@gmail.com>
2024-08-22 23:43:19 +0530

    done

--- src/main/java/lk/Ijse/config/FactoryConfiguration.java
+++ src/main/java/lk/Ijse/config/FactoryConfiguration.java
@@ -1,2 +1,27 @@
-package lk.Ijse.config;public class FactoryConfiguration {
+package lk.Ijse.config;
+
+import lk.Ijse.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+
+    private static SessionFactory sessionFactory;
+    private  FactoryConfiguration(){
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance(){
+        return (factoryConfiguration==null)?
+                factoryConfiguration= new FactoryConfiguration():factoryConfiguration;
+
+    }
+
+    public Session getSession(){
+        return sessionFactory.openSession();
+    }
 }
--- src/main/java/lk/Ijse/entity/Student.java
+++ src/main/java/lk/Ijse/entity/Student.java
@@ -1,2 +1,53 @@
-package lk.Ijse.entity;public class Student {
+package lk.Ijse.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lk.Ijse.embed.FullName;
+
+//@Entity(name = "k") meken aluth table ekaka hadenva
+@Entity
+//@Table(name = "l") mekenuth puluvn
+public class Student {
+    @Id
+    private int id;
+
+    private FullName name;
+
+    private  String address;
+
+    public Student(){
+
+    }
+
+    public Student(int id,String address ,FullName name) {
+        this.address = address;
+        this.id = id;
+        this.name=name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public FullName getName() {
+        return name;
+    }
+
+    public void setName(FullName name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
